30410a6ba6e3eb931e2faf75b186e7560bde692a
Mark Andro Guevarra <135146939+spacemark3@users.noreply.github.com>
2023-07-06 11:06:32 +0200

    Add files via upload

--- mailer.php
+++ mailer.php
@@ -14,8 +14,14 @@ $mail->SMTPAuth = true;
 $mail->Host = "smtp.gmail.com";
 $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
 $mail->Port = 587;
-$mail->Username = "mark";
-$mail->Password = "qwe";
+$mail->Username = "spacex";
+$mail->Password = "1234";
+
+$mail->setFrom('mark3andro@gmail.com', 'Mittente');
+$mail->addAddress('mark3andro@gmail.com', 'Destinatario');
+$mail->Subject = 'prova';
+$mail->Body = 'prova';
+
 
 $mail->isHtml(true);
 
