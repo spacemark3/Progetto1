09daccd1230e8388d60c3d458820cff7a618c0e3
Mark Andro Guevarra <135146939+spacemark3@users.noreply.github.com>
2023-07-07 18:07:42 +0200

    Update database.php

--- database.php
+++ database.php
@@ -18,7 +18,7 @@ $email = $_POST['email'];
 $telefono = $_POST['telefono'];
 $messaggio = $_POST['messaggio'];
 
-$to = 'mark3andro@gmail.com'; 
+$to = 'mark.guevarra@agmsolutions.net'; 
 $subject = 'Dati form ricevuti';
 $message = "Nome: $nome\n"
          . "Cognome: $cognome\n"
@@ -26,7 +26,7 @@ $message = "Nome: $nome\n"
          . "Telefono: $telefono\n"
          . "Messaggio: $messaggio";
 
-$headers = "From: mark3andro@gmail.com";
+$headers = "From: mark.guevarra@agmsolutions.net";
 $headers .= "\r\nReply-To: $email";
 
 $mailSent = false; 
@@ -53,6 +53,9 @@ $conn->close();
 <html>
 <head>
     <title>Message Button</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="style.css">
+  
 </head>
 <body>
     <?php if ($mailSent && $dbSaved): ?>
